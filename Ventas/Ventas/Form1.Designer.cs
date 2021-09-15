23c8e24adc17c68a28fabcba5fcf2755ddc01f5c
eliascldrn <elias.calderon@unah.hn>
2021-09-14 19:01:16 -0600

    Creando el programa hola mundo

--- Ventas/Ventas/Form1.Designer.cs
+++ Ventas/Ventas/Form1.Designer.cs
@@ -33,12 +33,13 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(88, 158);
+            this.button1.Location = new System.Drawing.Point(48, 93);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(110, 41);
+            this.button1.Size = new System.Drawing.Size(176, 67);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Procesar";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
--- Ventas/Ventas/Form1.cs
+++ Ventas/Ventas/Form1.cs
@@ -16,5 +16,11 @@ namespace vE
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            MessageBox.Show("Hola mundo");
+        }
     }
 }
